f4c7dea72580dab905d5201ac37a8b0c56c8d591
Cristian Marin <marin101998@gmail.com>
2024-10-15 18:38:06 -0500

    + fix | v10 15-10-24 add mutator status sensor

--- Entities/Log.php
+++ Entities/Log.php
@@ -10,9 +10,9 @@ class Log extends CrudModel
   public $transformer = 'Modules\Itelemetry\Transformers\LogTransformer';
   public $repository = 'Modules\Itelemetry\Repositories\LogRepository';
   public $requestValidation = [
-      'create' => 'Modules\Itelemetry\Http\Requests\CreateLogRequest',
-      'update' => 'Modules\Itelemetry\Http\Requests\UpdateLogRequest',
-    ];
+    'create' => 'Modules\Itelemetry\Http\Requests\CreateLogRequest',
+    'update' => 'Modules\Itelemetry\Http\Requests\UpdateLogRequest',
+  ];
   //Instance external/internal events to dispatch with extraData
   public $dispatchesEventsWithBindings = [
     //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
@@ -38,5 +38,26 @@ class Log extends CrudModel
   public function record()
   {
     return $this->belongsTo(Record::class);
+  }
+
+
+  public function getStatusSensorAttribute()
+  {
+    if (!empty($this->sensor->rules)) {
+      $rulesSensor = json_decode(json_encode($this->sensor->rules));
+      // Validacion valores del sensor
+//      $sensorRangeValues = $rulesSensor->range;
+//      if ($sensorRangeValues->min >= $this->value && $sensorRangeValues->max <= $this->value) {
+        foreach ($rulesSensor->rules as $rule) {
+          if ($rule->min <= $this->value && $rule->max >= $this->value) {
+            return $rule;
+          }
+        }
+//      } else {
+//        return null;
+//      }
+    } else {
+      return null;
+    }
   }
 }
